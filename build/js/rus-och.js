088303bb7b21eec6b46a309d4e339c2e8ec021e1
Nadia Degtyareva <degtyareva@ttcsoft.ru>
2017-04-23 23:26:26 +0300

    news.html

--- build/js/rus-och.js
+++ build/js/rus-och.js
@@ -1,6 +1,56 @@
 $(function(){
 
-
+	$('.news-slider-big').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '.news-slider-small'
+	});
+	$('.news-slider-small').slick({
+		slidesToShow: 6,
+		slidesToScroll: 1,
+		asNavFor: '.news-slider-big',
+		focusOnSelect: true,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1,
+					infinite: true,
+					dots: true
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+	
+	$('.js-comments-btn').on('click', function() {
+		$(this).hide();
+		$('.js-comments').show();
+	});
+	
+	$('.js-mistake-btn').on('click', function() {
+		$('.js-mistake').show();
+	});
+	
+	$('.js-search-btn').on('click', function() {
+		$('.js-search').fadeIn();
+	});
 
 });
 
